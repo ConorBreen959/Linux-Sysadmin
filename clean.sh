317345dee2b760e7c3045466ae4c8b56ba7b2932
ConorBreen959 <58226007+ConorBreen959@users.noreply.github.com>
2023-03-13 19:51:09 +0000

    Create clean.sh

---
+++ clean.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt-get upgrade
+
+sudo apt-get update
+
+sudo apt clean
+
+sudo apt -s clean
+
+sudo apt clean all
+
+sudo apt autoremove --purge
+
+sudo apt-get clean
+
+sudo apt-get -s clean
+
+sudo apt-get clean all
+
+sudo apt-get autoclean
+
+sudo apt-get autoremove --purge
+
+sudo journalctl --vacuum-time=3d
+
+sudo journalctl --vacuum-time=30d
+
+sudo journalctl --vacuum-time=300d
